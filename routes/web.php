f88ccf6ca3a8383ab0cbdaac014f423d6734286d
Matteo Perino <matte97.p@gmail.com>
2024-05-22 15:25:50 +0200

    login page

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', function () {
+    return view('login');
+});
+
 Route::get('/php', function () {
     echo phpinfo();
 });
